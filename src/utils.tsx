df4824b015bbc633ab3ffe2248b22be0126ec77a
pradanaadn <pradanaadn21@gmail.com>
2025-05-01 21:40:35 +0700

    Refactor getPath function to handle base path formatting and ensure consistent path resolution

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,5 +1,9 @@
 export function getPath(path: string): string {
-    const basePath = import.meta.env.BASE_URL;
-    const cleanPath = path.replace(/^\//, '');
-    return `${basePath}/${cleanPath}`;
+  const basePath = import.meta.env.BASE_URL || "";
+  const cleanPath = path.replace(/^\//, "");
+  if (basePath.endsWith("/")) {
+    return `${basePath}${cleanPath}`;
   }
+
+  return basePath ? `${basePath}/${cleanPath}` : `/${cleanPath}`;
+}
